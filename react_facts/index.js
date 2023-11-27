4fe1c04ca2bf7add5af281e2541dd53b7230d202
Gergely Kreko <gergely.kreko@nixs.com>
2023-11-27 10:36:59 +0100

    Make some react componenets

--- react_facts/index.js
+++ react_facts/index.js
@@ -1,1 +1,23 @@
-ReactDOM.render(<ul><li>first</li><li>second</li></ul>, document.getElementById("root"))
+function Navbar() {
+    return (
+        <nav>
+            <p>this is a navbar</p>
+        </nav>
+    );
+}
+
+function MainContent() {
+    return (
+    <div>
+        <h1>This is the main content</h1>
+    </div>
+    );
+}
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+)
