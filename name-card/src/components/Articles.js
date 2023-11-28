9a14c073d68b53fb26d5b66528cf99de34ef2a59
Gergely Kreko <gergely.kreko@nixs.com>
2023-11-28 11:39:28 +0100

    Bison name card with webpack and sass

---
+++ name-card/src/components/Articles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Article from './Article'
+
+export default function Articles(props) {
+    return (
+        <div>
+            <Article title="Description">
+                A bison has a shaggy, long, dark-brown winter coat, and a lighter-weight, lighter-brown summer coat. Male bison are significantly larger and heavier than females. Plains bison are often in the smaller range of sizes, and wood bison in the larger range. Head-rump lengths at maximum up to 3.5 m (11 ft 6 in) for males and 2.85 m (9 ft 4 in) for females long and the tail adding 30 to 95 cm (1 ft 0 in to 3 ft 1 in). Heights at withers in the species can reach up to 186 to 201 cm (6 ft 1 in to 6 ft 7 in) for B. b. bison and B. b. athabascae respectively. Typically weights can range from 318 to 1,179 kg (701 to 2,599 lb), 460 to 988 kg (1,014 to 2,178 lb) with medians of 730 to 792.5 kg (1,609 to 1,747 lb) (B.b. bison) and 943.6 kg (2,080 lb) (B.b.athabascae) in males, and 360 to 640 kg (790 to 1,410 lb) with medians of 450 to 497.6 kg (992 to 1,097 lb) in females, although the lowest weights probably representing typical weight around the age of sexual maturity at 2 to 3 years of age.
+            </Article>
+            <Article title="Evolution">
+                Bison are members of the tribe Bovini. Genetic evidence from nuclear DNA indicates that the closest living relatives of bison are yaks, with bison being nested within the genus Bos, rendering Bos without including bison paraphyletic. While nuclear DNA indicates that the two living bison species are each other's closest living relatives, the mitochondrial DNA of European bison is more closely related to that of domestic cattle and aurochs, which is either suggested to be the result of incomplete lineage sorting or ancient introgression. Bison first appeared in Asia during the Early Pleistocene, around 2.6 million years ago. Bison only arrived in North America 195,000 to 135,000 years ago, during the late Middle Pleistocene, descending from the widespread Siberian steppe bison (Bison priscus), which had migrated through Beringia. Following its first appearance in North America, the bison rapidly differentiated into new species such as the largest of all bison, the long-horned Bison latifrons as well as Bison antiquus. The first appearance of bison in North America is considered to define the regional Rancholabrean faunal stage, due to its major impact on the ecology of the continent. Modern American bison are thought to have evolved from B. antiquus during the Late Pleistocene-Holocene transition via the intermediate form Bison occidentalis.
+            </Article>
+        </div>
+    )
+}
