eceaaea0c1adb94c3c3fcd5d71f7a3b8c84aaf04
Gergely Kreko <gergely.kreko@nixs.com>
2023-11-30 11:20:16 +0100

    Make cards understand props

--- airbnb-experiences/src/components/Cards.js
+++ airbnb-experiences/src/components/Cards.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Card from "./Card";
+import swimmer from "../img/swimmer.jpg";
+
 
 export default function Cards() {
     return (
         <section className="cards">
-            <Card />
+            <Card
+                img={swimmer}
+                rating={5.0}
+                reviewCount={6}
+                country="USA"
+                title="Life lessons with this swimmer dude, who's name I do not know."
+                price={136}
+                soldOut={true}
+            />
         </section>
     )
 }
