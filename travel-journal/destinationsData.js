b030031a5f4bf9aa7adca9fda609f8155989cf79
Gergely Kreko <KrekoG@users.noreply.github.com>
2023-11-30 16:46:05 +0100

    Self assesment 2 (#3)
    * Fix key being a prop in pervious project, also adjust the namecard's backgroun colour
    
    * Add travel journal
    
    ---------
    
    Co-authored-by: Gergely Kreko <gergely.kreko@nixs.com>

---
+++ travel-journal/destinationsData.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        title: "Budapest",
+        location: "Hungary",
+        googleMapUrl: "https://www.google.com/maps/place/Budapest/@47.4807411,18.8006573,10z/data=!3m1!4b1!4m6!3m5!1s0x4741c334d1d4cfc9:0x400c4290c1e1160!8m2!3d47.497912!4d19.040235!16zL20vMDk1d18?entry=ttu",
+        startDate: "29 May, 2023",
+        endDate: null,
+        description: "The history of Budapest began when an early Celtic settlement transformed into the Roman town of Aquincum, the capital of Lower Pannonia. The Hungarians arrived in the territory in the late 9th century, but the area was pillaged by the Mongols in 1241–42. Re-established Buda became one of the centres of Renaissance humanist culture by the 15th century. The Battle of Mohács, in 1526, was followed by nearly 150 years of Ottoman rule. After the reconquest of Buda in 1686, the region entered a new age of prosperity, with Pest-Buda becoming a global city after the unification of Buda, Óbuda and Pest on 17 November 1873, with the name 'Budapest' given to the new capital. Budapest also became the co-capital of the Austro-Hungarian Empire, a great power that dissolved in 1918, following World War I. The city was the focal point of the Hungarian Revolution of 1848 and the Battle of Budapest in 1945, as well as the Hungarian Revolution of 1956.",
+        imageUrl: "budapest.jpg"
+    },
+    {
+        id: 2,
+        title: "Manchester",
+        location: "United Kingdom",
+        googleMapUrl: "https://www.google.com/maps/place/Manchester,+UK/@53.4721321,-2.3882701,11z/data=!3m1!4b1!4m6!3m5!1s0x487a4d4c5226f5db:0xd9be143804fe6baa!8m2!3d53.4807593!4d-2.2426305!16zL20vMDUyYnc?entry=ttu",
+        startDate: "4 July, 2014",
+        endDate: "29 May, 2023",
+        description: "The history of Manchester began with the civilian settlement associated with the Roman fort (castra) of Mamucium or Mancunium, established in about AD 79 on a sandstone bluff near the confluence of the rivers Medlock and Irwell. Throughout the Middle Ages, Manchester remained a manorial township but began to expand \"at an astonishing rate\" around the turn of the 19th century. Manchester's unplanned urbanisation was brought on by a boom in textile manufacture during the Industrial Revolution and resulted in it becoming the world's first industrialised city. Historically part of Lancashire, areas of Cheshire south of the River Mersey were incorporated into Manchester in the 20th century, including Wythenshawe in 1931. Manchester achieved city status in 1853. The Manchester Ship Canal opened in 1894, creating the Port of Manchester and linking the city to the Irish Sea, 36 miles (58 km) to the west. Its fortune declined after the Second World War, owing to deindustrialisation, and the IRA bombing in 1996 led to extensive investment and regeneration. Following considerable redevelopment, Manchester was the host city for the 2002 Commonwealth Games.",
+        imageUrl: "manchester.jpg"
+    }
+]
---
+++ travel-journal/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <nav className="navbar">
+            <h1 className="navbar-title">Travel journal</h1>
+        </nav>
+    )
+}
