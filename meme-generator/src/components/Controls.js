4140218ac0c4f6b4ce5359350fca6657d987009a
Gergely Kreko <gergely.kreko@nixs.com>
2023-12-01 11:46:15 +0100

    Add some basic event handling

--- meme-generator/src/components/Controls.js
+++ meme-generator/src/components/Controls.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 export default function Controls() {
+    function buttonClickHandler() {
+        console.log('the button was clicked!')
+    }
+
+    function hoverHandler() {
+        console.log('the hover has happened!')
+    }
+
     return (
         <section>
             <div className="controls-text-container">
@@ -10,12 +18,14 @@ export default function Controls() {
                     placeholder="Top text"
                 />
                 <input
+                    onMouseOver={hoverHandler}
                     className="controls-text"
                     type="text"
                     placeholder="Bottom text"
                 />
             </div>
             <input
+                onClick={buttonClickHandler}
                 className="controls-get-new-button"
                 type="button"
                 value="Get a new meme image 🖼️"
