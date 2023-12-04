f68659f4b733f936b4c635ec109dc56ee6941efd
Gergely Kreko <gergely.kreko@nixs.com>
2023-12-04 14:21:30 +0100

    Add a Show punchline button

--- jokes/src/components/Joke.js
+++ jokes/src/components/Joke.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+
+    function toggleShown() {
+        setIsShown(prev => !prev)
+    }
+
     return (
         <div className="joke">
             {props.setup && <p>{props.setup}</p>}
-            <p>{props.punchline}</p>
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggleShown}>
+                {`${isShown? "Hide" : "Show"} Punchline`}
+            </button>
         </div>
     )
 }
