d10a62880f1b435fc690c9e940f9a7b858641a43
Gergely Kreko <gergely.kreko@nixs.com>
2023-12-05 12:31:23 +0100

    Make the button load the next character

--- simple-api-call/src/App.js
+++ simple-api-call/src/App.js
@@ -4,14 +4,14 @@ import './css/main.scss'
 
 export default function App() {
     const [starWarsData, setStarWarsData] = React.useState({})
-    const [count, setCount] = React.useState(0)
+    const [count, setCount] = React.useState(1)
 
     function handleClick() {
         setCount(prev => prev + 1)
     }
 
     React.useEffect(() => {
-        fetch("https://swapi.dev/api/people/1")
+        fetch(`https://swapi.dev/api/people/${count}`)
             .then(res => res.json())
             .then(data => setStarWarsData(data))
         console.log("Effect function ran!");
@@ -22,7 +22,7 @@ export default function App() {
     return (
         <main>
             <p>The count is {count}</p>
-            <button onClick={handleClick}>+1, and reload data</button>
+            <button onClick={handleClick}>Get Next Character</button>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </main>
     )
