ef535a256f9687f6cb268982d9418c941f3bffba
Gergely Kreko <gergely.kreko@nixs.com>
2023-12-05 14:34:35 +0100

    Make the watcher shorter

--- window-tracker/src/components/WindowTracker.js
+++ window-tracker/src/components/WindowTracker.js
@@ -4,15 +4,12 @@ export default function WindowTracker() {
     const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
 
     React.useEffect(() => {
-        function watchWidth() {
-            setWindowWidth(window.innerWidth)
-        }
+        const watchWidth = () => setWindowWidth(window.innerWidth)
         window.addEventListener('resize', watchWidth)
         return () => {
             window.removeEventListener('resize', watchWidth)
         }
     }, [])
-
 
     return (
         <h1>Window width: {windowWidth}</h1>
